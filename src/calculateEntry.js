d867554f5cf70a6dd9d3ccf9ed9e1dc6c93841cf
Patricia Eliseu <patyeliseu4@gmail.com>
2022-03-16 14:47:52 -0300

    fazendo o pullrequest

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,9 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function countEntrants(entrants) { // seu código aqui
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants) { // seu código aqui
 }
 
 module.exports = { calculateEntry, countEntrants };
