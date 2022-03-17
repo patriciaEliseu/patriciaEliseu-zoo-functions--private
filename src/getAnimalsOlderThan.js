04b272ae75359ccad7226acc44c0dc03de22a231
Patricia Eliseu <patyeliseu4@gmail.com>
2022-03-17 18:45:21 -0300

    add requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const acharAnimal = data.species.find((residents) => animal.includes(residents.name));
+  const verifica = acharAnimal.residents.every((elemento) => elemento.age > age);
+  return verifica;
 }
-
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
