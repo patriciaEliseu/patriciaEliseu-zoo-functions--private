4a9b44d6a86c42b1b5b14dc6dc8cbf2650e16025
Patricia Eliseu <patyeliseu4@gmail.com>
2022-03-23 21:17:22 -0300

    add requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const buscarColaborador = data.employees.find((elemento) => elemento.id === id); /* achei a pessoa colab do param id */
+  const animalGerenciado = buscarColaborador.responsibleFor[0]; /* achei a primeira especie geren pelo colaborador */
+  const animalEspecie = data.species.find((elem) => elem.id === animalGerenciado); /* achei a especie */
+  const maisVelho = animalEspecie.residents.sort((a, b) => (b.age - a.age));
+  // console.log(maisVelho[0]);
+  return Object.values(maisVelho[0]);
 }
-
+// getOldestFromFirstSpecies();
 module.exports = getOldestFromFirstSpecies;
